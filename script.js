ad1ea8b2ed0cac51a72941cea62a0c7cce3ebf0e
Agrawal Dhruv Mahesh <adhruvbtech21@ced.alliance.edu.in>
2024-08-01 11:46:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Add a simple animation to the profile image
-const profileImage = document.querySelector('.profile-image');
-profileImage.addEventListener('mouseover', () => {
-    profileImage.classList.add('animated');
-});
-profileImage.addEventListener('mouseout', () => {
-    profileImage.classList.remove('animated');
-});
